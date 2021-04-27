c6e329268f8bcd4e0d19f0eed7f480a5757517f1
Witoon Ruamngoen <witoonaoo@gmail.com>
2021-04-27 21:15:01 +0700

    First Upload
    This is the first upload of my project.

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+    // mongoose = require('mongoose');
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+// mongoose.connect('mongodb://localhost/DogeAir');
+// var dogeairSchrma = new mongoose.Schema({
+//     name: String,
+//     image: String,
+//     desc: String
+// });
+
+// var Dogeair = mongoose.model('Dogeair', dogeairSchrma);
+
+// Dogeair.create(
+//     {
+//         name: "Doge1",
+//         image: "http://i.imgur.com/1pLHYey.jpg",
+//         desc: 'Such plane'
+//     },
+//     function(err, dogeair) {
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log('New data added');
+//             console.log(dogeair);
+//         }
+        
+//     }
+// );
+
+app.get('/', function(req,res){
+    res.render("home.ejs")
+});
+
+app.get('/flights', function(req,res){
+    res.render("flights.ejs")
+});
+
+app.get('/sign-in', function(req,res){
+    res.render("sign-in.ejs")
+});
+app.get('/sign-up', function(req,res){
+    res.render("sign-up.ejs")
+});
+
+app.get('*', function(req,res){
+    res.send('Bad request.')
+})
+
+app.listen('3000', function(req, res){
+    console.log('Server is running');
+});
